e948c3451d205e99ad4fea7ac8832fa690487a6f
Stefan Nieuwenburg <82890714+Stefannieuwenburg@users.noreply.github.com>
2021-05-28 20:59:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("hallo winc academy studenten")
+let sum = 1 + 1;
+console.log("sum:", sum);
+//dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division);
+
+
+/*dit is een blok commant
+
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division);
+
+*/
+
